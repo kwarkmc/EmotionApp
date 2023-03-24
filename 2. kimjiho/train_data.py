6825e88fe1cbff3e7f0b2e64d04e561f4d38b2e6
kjh-0523 <124140035+kjh-0523@users.noreply.github.com>
2023-03-24 12:04:12 +0900

    Update train_data.py

--- 2. kimjiho/train_data.py
+++ 2. kimjiho/train_data.py
@@ -16,17 +16,17 @@ def df_maker(col_num, ind_num, fill):
         con.append(col)
     return pd.DataFrame(con, columns = col, index = ind)
 
-df = df_maker(2,50,0)
+df = df_maker(2,500,0)
 df.columns = ["대사","라벨"]
 while(1):
     i = i + 1
     i_str = str(i)
-    if os.path.isfile(r"D:\015.감성 및 발화 스타일별 음성합성 데이터\01.데이터\2.Validation\라벨링데이터\VL1\4.감정X발화스타일\9.상처X구연체\0047_G2A4E5S1C0_LSJ\0047_G2A4E5S1C0_LSJ_"+i_str.zfill(6)+".json"):
+    if os.path.isfile(r"D:\015.감성 및 발화 스타일별 음성합성 데이터\01.데이터\1.Training\라벨링데이터\TL1\TL1\1.감정\3.분노\0001_G1A3E3S0C0_PSB\0001_G1A3E3S0C0_PSB_"+i_str.zfill(6)+".json"):
         with open(
-                r"D:\015.감성 및 발화 스타일별 음성합성 데이터\01.데이터\2.Validation\라벨링데이터\VL1\4.감정X발화스타일\9.상처X구연체\0047_G2A4E5S1C0_LSJ\0047_G2A4E5S1C0_LSJ_" + i_str.zfill(6) + ".json", encoding='UTF-8') as f:
+                r"D:\015.감성 및 발화 스타일별 음성합성 데이터\01.데이터\1.Training\라벨링데이터\TL1\TL1\1.감정\3.분노\0001_G1A3E3S0C0_PSB\0001_G1A3E3S0C0_PSB_" + i_str.zfill(6) + ".json", encoding='UTF-8') as f:
             json_object = json.load(f)
     df.iloc[i-1] = [json_object["전사정보"]["OrgLabelText"],0]
-    if(i==50):
+    if(i==500):
         break
 
-df.to_csv(r"D:\test\rating_test(상처).txt",sep = '\t',index = False)
+df.to_csv(r"D:\test\rating_train(분노).txt",sep = '\t',index = False)
